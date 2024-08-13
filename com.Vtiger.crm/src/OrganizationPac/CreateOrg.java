e9ca3e2914056457675317a76fff029427be95af
hp <hp@DESKTOP-0QBGJSL>
2024-08-13 14:12:30 +0530

    create org is  added

--- com.Vtiger.crm/src/OrganizationPac/CreateOrg.java
+++ com.Vtiger.crm/src/OrganizationPac/CreateOrg.java
@@ -3,6 +3,8 @@ package OrganizationPac;
 public class CreateOrg {
 	public static void main(String[] args) {
 		System.out.println("=====Create org====");
+		System.out.println("=====Createorg====");
+		
 	}
 
 }
