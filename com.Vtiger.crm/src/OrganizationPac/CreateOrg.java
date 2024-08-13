86ea82a03285cbc8d8a2b33555c04aed8a502b90
Nsuma24 <nsuma242000@gmail.com>
2024-08-13 14:24:46 +0530

    Update CreateOrg.java

--- com.Vtiger.crm/src/OrganizationPac/CreateOrg.java
+++ com.Vtiger.crm/src/OrganizationPac/CreateOrg.java
@@ -3,6 +3,7 @@ package OrganizationPac;
 public class CreateOrg {
 	public static void main(String[] args) {
 		System.out.println("=====Create org====");
+		System.out.println("=====Create org1====");
 		
 		
 	}
